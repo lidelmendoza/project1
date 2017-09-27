e8669c65200603626082b8ee731f05efe1f3ab60
lidelmendoza <lidelmendoza9@csu.fullerton.edu>
2017-09-27 16:30:49 -0700

    Update GroceryBill.h
    deleted doubleArray

--- GroceryBill.h
+++ GroceryBill.h
@@ -14,7 +14,6 @@ public:
 	GroceryBill(const PriceList *priceList, double taxRate);
 	void scanItem(string scanCode, double quantity); // add item and quantity to bill; throw exception if item's code is not found in the pricelist
 	void scanItemsFromFile(string filename); // Scan multiple codes and quantities from the given text file
-    	void doubleArray();
 	void printBill(); // Print the bill to cout. Each line contains the name of an item, total price, and the letter "T" if tax was addded.
     double getTotal(); // return the total cost of all items scanned
 	~GroceryBill(){delete[] bill;} //Delete the ptr
