ddaa4ab7ec82a8e53543745e74cfacb0469dbac1
lidelmendoza <lidelmendoza9@csu.fullerton.edu>
2017-09-27 16:45:18 -0700

    Update PriceList.h

--- PriceList.h
+++ PriceList.h
@@ -7,16 +7,16 @@ using namespace std;
 
 class PriceList {
 public:
-	
-	void createPriceListFromDatafile(string filename); // Load information from a text file with the given filename (Completed)
-	void addEntry(string itemName, string code, double price, bool taxable); // add to the price list information about a new item. A max of 1,000,000 entries can be added
-	bool isValid(string code) const; // return true only if the code is valid
-	PriceListItem getItem(string code) const; // return price, item name, taxable? as an PriceListItem object; throw exception if code is not found
-
+    PriceList();
+    PriceList(const PriceList &pl);
+    PriceList& operator=(const PriceList& pl);
+	void createPriceListFromDatafile(string filename);//reads text file
+	void addEntry(string itemName, string code, double price, bool taxable);// stores data from text file into program
+    void doubleArray();
+	bool isValid(string code) const;
+	PriceListItem getItem(string code) const;
+     ~PriceList(){delete[] aptr;} //Delete the ptr
 private:
-	// Add private member variables for your class along with any 
-	// other variables required to implement the public member functions
-	// TO BE COMPLETED
-
-
+    int index, size;
+	PriceListItem *aptr;
 };
